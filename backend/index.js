6a458ce2200b099f47b283d286dcc78f213cd8d8
Abhishekpandit888 <pandit.abhishek1812@gmail.com>
2024-08-03 18:28:38 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,13 @@ import { fileURLToPath } from "url";
 
 //deploy start
 const app = express();
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// app.use(express.static(path.join(__dirname, "/frontend/build")));
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
+});
 
 ///deploy end
 
